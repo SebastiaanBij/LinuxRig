e6526ac663c0ea989c90262fe86297b51fb3f59e
Sebastiaan Bij <71077764+SebastiaanBij@users.noreply.github.com>
2022-11-18 11:23:14 +0100

    Update setup-rig.sh

--- setup/setup-rig.sh
+++ setup/setup-rig.sh
@@ -10,7 +10,7 @@ echo "Downloading & Executing Firmware Script..."
 curl -s "https://raw.githubusercontent.com/SebastiaanBij/LinuxRig/debian-bullseye/firmware-install.sh" | bash
 
 echo "Prepping Reboot Continuation..."
-echo "curl -s 'https://raw.githubusercontent.com/SebastiaanBij/LinuxRig/debian-bullseye/kde-install.sh' | bash"
+echo "curl -s 'https://raw.githubusercontent.com/SebastiaanBij/LinuxRig/debian-bullseye/setup/setup-rig-continuation.sh' | bash" >> /root/.profile
 
 echo "Rebooting..."
 reboot now
